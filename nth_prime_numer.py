206f3bce00e932de251f5cce2e48bf23f2aaf889
Thao Huynh <34813925+thhuynh91@users.noreply.github.com>
2018-09-22 11:11:15 -0700

    Update nth_prime_numer.py

--- nth_prime_numer.py
+++ nth_prime_numer.py
@@ -4,8 +4,6 @@ Ex: 6th prime number is 13 (2,3,5,7,11,13)
 def nth_prime_number(num):
     prime = [2]
     x = 3
-    if num < 2:
-        return 2
     while len(prime) < num:
         for y in prime:
             if x%y == 0:
